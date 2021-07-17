c037978a17994bd054c7422b450078c1393244f7
Fyre <sushi.wolf@hotmail.com>
2021-07-17 04:55:43 -0700

    Add basic mana UI

---
+++ Jam/Assets/Scripts/UI/Displays/HealthDisplay.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class HealthDisplay : MonoBehaviour
+{
+    [Header("Data")]
+    public EntityData EntityData;
+    public ColorVariable HealthColor;
+    public ColorVariable GraceColor;
+    public ColorVariable GritColor;
+
+    [Header("Images")]
+    public Image HealthBar;
+    public Image GraceBar;
+    public Image GritBar;
+
+    [Header("Text")]
+    public TextMeshProUGUI HealthText;
+    public TextMeshProUGUI GraceText;
+    public TextMeshProUGUI GritText;
+
+    public void Start()
+    {
+        HealthBar.color = HealthColor.Value;
+        GraceBar.color = GraceColor.Value;
+        GritBar.color = GritColor.Value;
+
+        GraceText.outlineColor = GraceColor.Value;
+        GritText.outlineColor = GritColor.Value;
+    }
+
+    public void FixedUpdate()
+    {
+        UpdateHealthBar();
+        UpdateGraceAndGrit();
+        UpdateText();
+    }
+
+    private void UpdateHealthBar()
+    {
+        if (EntityData.Health.Value > EntityData.Health.Max)
+        {
+            HealthBar.transform.localPosition = new Vector3(0, 0, 0);
+        }
+        else
+        {
+            HealthBar.transform.localPosition = new Vector3(
+            -1 * (1 - (EntityData.Health.Value / EntityData.Health.Max)) * HealthBar.rectTransform.rect.width,
+            HealthBar.transform.localPosition.y,
+            HealthBar.transform.localPosition.z);
+        }
+    }
+
+    private void UpdateGraceAndGrit()
+    {
+        GraceBar.enabled = EntityData.Grace.Value > EntityData.Grace.Min;
+        GritBar.enabled = EntityData.Grit.Value > EntityData.Grit.Min;
+
+        GraceText.enabled = EntityData.Grace.Value > EntityData.Grace.Min;
+        GritText.enabled = EntityData.Grit.Value > EntityData.Grit.Min;
+    }
+
+    private void UpdateText()
+    {
+        HealthText.text = EntityData.Health.Value + " / " + EntityData.Health.Max;
+        GraceText.text = EntityData.Grace.Value.ToString();
+        GritText.text = EntityData.Grit.Value.ToString();
+    }
+}
---
+++ Jam/Assets/Scripts/UI/Displays/ManaDisplay.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class ManaDisplay : MonoBehaviour
+{
+    [Header("Data")]
+    public EntityData EntityData;
+    public ColorVariable ManaColor;
+
+    [Header("Images")]
+    public Image ManaIris;
+
+    [Header("Text")]
+    public TextMeshProUGUI ManaValueText;
+    public TextMeshProUGUI ManaGainText;
+
+    public void Start()
+    {
+        ManaIris.color = ManaColor.Value;
+    }
+
+    public void Update()
+    {
+        ManaValueText.text = EntityData.Mana.Value.ToString();
+        ManaGainText.text = EntityData.ManaGain.ToString();
+    }
+}
